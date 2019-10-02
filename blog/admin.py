d0d46fba9b24a9ccd9aa87ce86baed6a096c7b95
jy <wnsdud3119@gmail.com>
2019-10-02 16:12:39 +0900

    댓글기능 추가/ 관리자 댓글관리

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
+# blog/admin.py
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-
+# mysite/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
