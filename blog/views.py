c0ad8f4b49b438b9028841abb26f9ac5a8038970
jy <wnsdud3119@gmail.com>
2019-10-04 17:01:59 +0900

    카카오로그인 배포v1.1 redirect uri수정

--- blog/views.py
+++ blog/views.py
@@ -96,7 +96,7 @@ def add_comment_to_post(request, pk):
             login_request_uri = 'https://kauth.kakao.com/oauth/authorize?'
 
             client_id = '63e4734e72d2d421ef9d5ff9200a241f'
-            redirect_uri = 'http://127.0.0.1:8000/oauth'
+            redirect_uri = 'http://wnsdud3119.pythonanywhere.com/oauth'
             post_primary_key = pk
             login_request_uri += 'client_id=' + client_id
             login_request_uri += '&redirect_uri=' + redirect_uri
@@ -158,4 +158,5 @@ def oauth(request):
     # print(userinfo_json)
     # print(User.objects.all())
 
-    return redirect('http://127.0.0.1:8000/post/{}/comment/'.format(post_primary_key))
+    #return redirect('http://127.0.0.1:8000/post/{}/comment/'.format(post_primary_key))
+    return redirect('http://wnsdud3119.pythonanywhere.com/post/{}/comment/'.format(post_primary_key))
