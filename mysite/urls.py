1d52ef8805175ce21a4530227c21949ac90fb1c9
jy <wnsdud3119@gmail.com>
2019-10-07 08:41:43 +0900

    logout next_page(/)추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,7 @@ import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout', kwargs={'next_page': ''}),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
     path('oauth/', blog.views.oauth, name='oauth'),
 ]
